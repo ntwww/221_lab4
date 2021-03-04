f48d6ad2856ca8a3af9e8bafd56a6f119c2cc2d6
ntwww <nattawut.rod@ku.th>
2021-03-04 15:55:55 +0700

    mental 3

--- 6320502398_4_1.c
+++ 6320502398_4_1.c
@@ -1,17 +1,22 @@
 #include<Stdio.h>
 int main()
 {
-    int p,i,j,k;
+    int p,i,j;
     scanf("%d",&p);
     int x[3][p],sum[p],real[p-2];
-    int lim = p-2;
-    for(i=0; i<3; i++)
+    for(i=0; i<p; i++)
     {
         sum[i]=0;
-        for(j=0; j<p; j++)
+        for(j=0; j<3; j++)
         {
             scanf("%d",&x[i][j]);
-            if (j==0)
+        }
+    }
+for(i=0; i<p; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+                    if (j==0)
             {
                 x[i][j]=x[i][j]*4;
             }
@@ -19,27 +24,33 @@ int main()
             {
                 x[i][j]=x[i][j]*2;
             }
-            sum[i]+=x[i][j];
+            else if (j==2)
+            {
+                x[i][j]=x[j][j]*1;
+            }
+            sum[i]=sum[i]+x[i][j];
         }
     }
-    for(i=0; i<lim; i++)
+    for(i=0; i<p-2; i++)
     {
-        real [i]= sum[i]+sum[i+1]+sum[i+2];
+        real[i]=0;
+        real[i]= (sum[i])+(sum[i+1])+(sum[i+2]);
     }
-    int max=real[0],min=real[0],best,bad;
-    for(i=1;i<lim;i++)
+    int max=real[0],min=real[0],best=0,bad=0;
+    for(i=0;i<p-2;i++)
     {
+        printf("%d ",real[i]);
         if(max<real[i])
         {
             max=real[i];
             best=i;
         }
-        if(min>real[i])
+        else if(min>real[i])
         {
             min=real[i];
             bad=i;
         }
     }
-    printf("%d %d",best+1,min+1);
-
+    printf("%d %d",best+1,bad+1);
+}
 
