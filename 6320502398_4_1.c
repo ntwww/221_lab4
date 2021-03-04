b47dd8a0d4fb9e57e534c86b25b2f6a63572135c
ntwww <nattawut.rod@ku.th>
2021-03-04 14:59:03 +0700

    Medal1

---
+++ 6320502398_4_1.c
@@ -0,0 +1,13 @@
+ #include<Stdio.h>
+ int main()
+ {
+ int p,i,j;
+ scanf("%d",&p);
+ int x[3][p];
+ for(i=0;i<3;i++)
+    {
+        for(j=0;j<p;j++)
+            scanf("%d",&x[i][j]);
+
+    }
+ }
